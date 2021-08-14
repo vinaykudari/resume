e7f60da206f888012cb59c73cbbd6f7455b2be51
Vinay Kudari <vinay@braveorbit.io>
2021-08-15 02:31:45 +0530

    Change Engine

--- actions/xelatex/entrypoint.sh
+++ actions/xelatex/entrypoint.sh
@@ -7,7 +7,7 @@ mkdir --parent $OUT_DIR
 for fileName in *.tex; do
     [ -f "$fileName" ] || break
     echo "Converting file $fileName to pdf..."
-    pdftex $fileName
+    xelatex $fileName
 done
 
 cp *.pdf $OUT_DIR 2>/dev/null || :
